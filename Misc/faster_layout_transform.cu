8283df524328af961929661ffcc2abfd6486d8dd
Andrew Zhao Luo <andrew.zhao.luo@gmail.com>
2023-02-15 13:26:24 -0800

    faster layout transform

--- Misc/faster_layout_transform.cu
+++ Misc/faster_layout_transform.cu
@@ -4,8 +4,10 @@ Playing around with implementing layout transform / transposition efficiently
 #include <assert.h>
 #include <cuda.h>
 #include <stdio.h>
+#include <stdexcept>
 
 #define COPY_LOADS_PER_THREAD 1
+
 __global__ void CudaBaselineCopy(float *mat1, float *mat2) {
   int start_index_t0 = blockIdx.x * blockDim.x * COPY_LOADS_PER_THREAD;
   for (int i = 0; i < COPY_LOADS_PER_THREAD; i++) {
@@ -31,6 +33,31 @@ __global__ void CudaTransposeNaive(float *mat, float *matT, int rows, int cols,
   }
 }
 
+template<int tile_size=32, int padding_shared=0>
+__global__ void CudaTransposeSharedIntermediate(float *mat, float *matT, int rows, int cols) {
+  // num threads = tile_size each thread will load and thread tile_size elements.
+  __shared__ float tile[tile_size][tile_size + padding_shared]; 
+
+  // index in mat 
+  int y_start = blockIdx.y * tile_size;
+  int x_start = blockIdx.x * tile_size + threadIdx.x;
+
+  // cooperatively load shared memory, do not transpose yet 
+  for (int i = 0; i < tile_size; i++) {
+    tile[i][threadIdx.x] = mat[(y_start + i) * cols + x_start];
+  }
+
+  __syncthreads();
+
+  // index in matT 
+  x_start = blockIdx.y * tile_size + threadIdx.x;
+  y_start = blockIdx.x * tile_size;
+  for (int i = 0; i < tile_size; i++) {
+    // matT[x_start * rows + y_start + i] = tile[i][threadIdx.x];
+    matT[(y_start + i) * rows + x_start] = tile[threadIdx.x][i];
+  }
+}
+
 void CudaTransposeNaiveWrapped(float *d_mat, float *d_matT, int rows, int cols,
                                int loads_per_thread = 32,
                                int num_threads_per_block = 32) {
@@ -44,7 +71,19 @@ void CudaTransposeNaiveWrapped(float *d_mat, float *d_matT, int rows, int cols,
       d_mat, d_matT, rows, cols, loads_per_thread, num_threads_per_block);
 }
 
-void doTransposeCUDA(float *h_mat, float *h_matT, int rows, int cols) {
+template<int tile_size=32, int padding_shared=0>
+void CudaTransposeIntermediateWrapped(float *d_mat, float *d_matT, int rows, int cols) {
+  int num_blocks_y = rows / tile_size;
+  assert(rows % tile_size == 0);
+  int num_blocks_x = cols / tile_size;
+  assert(cols % tile_size == 0);
+  dim3 dimBlock(num_blocks_x, num_blocks_y, 1);
+  dim3 dimGrid(tile_size, 1, 1);
+  CudaTransposeSharedIntermediate<tile_size, padding_shared><<<dimBlock, dimGrid>>>(
+      d_mat, d_matT, rows, cols);
+}
+
+void doTransposeCUDA(float *h_mat, float *h_matT, int rows, int cols, int mode=0) {
   int matrix_size = rows * cols * sizeof(float);
   float *d_mat, *d_matT;
 
@@ -54,8 +93,20 @@ void doTransposeCUDA(float *h_mat, float *h_matT, int rows, int cols) {
   cudaMemcpy(d_matT, h_matT, matrix_size, cudaMemcpyHostToDevice);
 
   // Call kernel
-  CudaTransposeNaiveWrapped(d_mat, d_matT, rows, cols, 4, 1024);
-
+  switch(mode) {
+    case 0:
+      CudaTransposeNaiveWrapped(d_mat, d_matT, rows, cols, 4, 1024);
+      break;
+    case 1:
+      CudaTransposeIntermediateWrapped<32, 0>(d_mat, d_matT, rows, cols);
+      break;
+    case 2:
+      CudaTransposeIntermediateWrapped<32, 1>(d_mat, d_matT, rows, cols);
+      break;
+    default:
+      printf("Error unknown mode %d\n", mode);
+      throw std::invalid_argument("Unknown mode");
+  }
   cudaMemcpy(h_matT, d_matT, matrix_size, cudaMemcpyDeviceToHost);
   cudaFree(d_mat);
   cudaFree(d_matT);
@@ -69,7 +120,7 @@ void doTransposeCPU(float *h_mat, float *h_matT, int rows, int cols) {
   }
 }
 
-void runTransposeExperiment(int matrix_rows, int matrix_cols) {
+void runTransposeExperiment(int matrix_rows, int matrix_cols, int mode) {
   float *h_mat = (float *)malloc(sizeof(float) * matrix_rows * matrix_cols);
   float *h_matT_experimental =
       (float *)malloc(sizeof(float) * matrix_rows * matrix_cols);
@@ -86,7 +137,7 @@ void runTransposeExperiment(int matrix_rows, int matrix_cols) {
   double cpu_time_used;
 
   start = clock();
-  doTransposeCUDA(h_mat, h_matT_experimental, matrix_rows, matrix_cols);
+  doTransposeCUDA(h_mat, h_matT_experimental, matrix_rows, matrix_cols, mode);
   cudaDeviceSynchronize();
   end = clock();
   cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
@@ -135,8 +186,12 @@ void runCopyThroughputExperiment(int matrix_rows, int matrix_cols) {
   }
 }
 
-int main() {
+int main(int argc, char** argv) {
+  int mode = 0;
+  if (argc >= 2) {
+    mode = atoi(argv[1]);
+  }
   runCopyThroughputExperiment(1024, 1024);
-  runTransposeExperiment(1024, 1024);
+  runTransposeExperiment(1024, 1024, mode);
   return 0;
 }
